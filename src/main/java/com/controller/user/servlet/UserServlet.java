b091eedcf8d10352bb4870572eef888a97090714
your username <theibdn159555@gmail.com>
2021-03-29 01:25:24 +0700

    Add key C

--- src/main/java/com/controller/user/servlet/UserServlet.java
+++ src/main/java/com/controller/user/servlet/UserServlet.java
@@ -78,7 +78,7 @@ public class UserServlet extends HttpServlet {
 
 		// check db
 		String message = UserDAO.insertUser(userInfo);
-		System.out.println("* : newbranch " + message);
+		System.out.println("* : key C" + message);
 		// main
 
 		// res
