344195843fac3d80b741653545cee865525b7bfa
your username <theibdn159555@gmail.com>
2021-03-28 22:10:59 +0700

    dang commit

--- src/main/java/com/controller/user/servlet/UserServlet.java
+++ src/main/java/com/controller/user/servlet/UserServlet.java
@@ -78,7 +78,7 @@ public class UserServlet extends HttpServlet {
 
 		// check db
 		String message = UserDAO.insertUser(userInfo);
-		System.out.println("* : test pull" + message);
+		System.out.println("* : dang commit" + message);
 		// main
 
 		// res
