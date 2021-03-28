21f9ab8b7a86aa8d1dd5581800f263f23ae279fc
theibdn159555 <60897161+theibdn159555@users.noreply.github.com>
2021-03-29 01:58:59 +0700

    Update UserServlet.java

--- src/main/java/com/controller/user/servlet/UserServlet.java
+++ src/main/java/com/controller/user/servlet/UserServlet.java
@@ -78,7 +78,7 @@ public class UserServlet extends HttpServlet {
 
 		// check db
 		String message = UserDAO.insertUser(userInfo);
-		System.out.println("* : key C" + message);
+		System.out.println("* : key D" + message);
 		// main
 
 		// res
