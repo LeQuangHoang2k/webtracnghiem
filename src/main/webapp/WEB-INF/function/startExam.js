df193eda3040c6fd7dcf698ab785e63f9982b71a
your username <theibdn159555@gmail.com>
2021-04-18 12:21:08 +0700

    pull cho trung

--- src/main/webapp/WEB-INF/function/startExam.js
+++ src/main/webapp/WEB-INF/function/startExam.js
@@ -1,15 +1,29 @@
 const mysql = require("mysql2");
 
 exports.startExam = (io, socket, data) => {
-  console.log(data.roomId);
-  const { roomId } = data;
+  const { roomId, optionExam } = data;
+  console.log(roomId, optionExam);
   //
 
+  const { option } = getOptionExam(optionExam);
+  console.log("option", option);
   //
 
   //
 
   // response
-  console.log(roomId);
   io.in(roomId).emit("all-member-start-success");
+};
+
+const getOptionExam = (optionExam) => {
+  let array = [];
+  let option = {};
+
+  array = optionExam.split("&");
+  array.forEach((item) => {
+    item = item.split("=");
+    option[item[0]] = item[1];
+  });
+
+  return { option };
 };
